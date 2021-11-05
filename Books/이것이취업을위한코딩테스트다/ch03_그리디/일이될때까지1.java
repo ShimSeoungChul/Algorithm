b1ec405452fad4323e60c6fbb43235d059b9a1ba
Shim Seoung Chul <tlatmdcjf94@gmail.com>
2021-11-05 16:02:15 +0900

    그리디 알고리즘 - 일이될떄까지1 코드 개선

--- Books/이것이취업을위한코딩테스트다/ch03_그리디/일이될때까지1.java
+++ Books/이것이취업을위한코딩테스트다/ch03_그리디/일이될때까지1.java
@@ -2,23 +2,22 @@
 import java.util.Scanner;
 
 public class 일이될때까지1 {
-    static public void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();  // 2 <= N <= 100,000
-        int K = scanner.nextInt();;  // 2 <= K <= 100,000
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();  // 2 <= N <= 100,000
+        int K = sc.nextInt();;  // 2 <= K <= 100,000
 
-        int count = 0; //실행 횟수
+        int count = 0; // 연산을 한 횟수
 
-        while(N!=1){    // N이 1이 될 떄까지 반
-            // N이 K로 나누어 떨어질 경우 두 번째 연산 수행, 아닐 경우 첫 번째 연산 수행
-            if(N == K*(N/K)){  // 조건을 (N % K == 0)으로 했으면 더 깔끔했음.
+        // N이 1이 되면 연산 종료
+        while (N != 1){
+            // N이 K로 나누어 떨어질 경우 나누고,
+            if(N%K == 0){
                 N = N/K;
-                count ++;
-                continue; // 두 번째 연산 종료 후 반복문의 처음으로 돌아가기
+            }else{ // 아닐 경우 1을 뺴기
+                N -= 1;
             }
-
-            N = N -1;
-            count ++;
+            count++;
         }
 
         System.out.println(count);
