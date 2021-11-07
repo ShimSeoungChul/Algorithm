f1b7b5aa061dc015464623efde0898f034bc4cc3
Shim Seoung Chul <tlatmdcjf94@gmail.com>
2021-11-07 18:29:38 +0900

    구현 - 시각 문제풀이

--- Books/이것이취업을위한코딩테스트다/ch04_구현/시각.java
+++ Books/이것이취업을위한코딩테스트다/ch04_구현/시각.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class 시각 {
+import java.util.Scanner;
+
+// TODO: 다시 풀기
+public class 시각 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count = 0;
+        for(int i=0; i <= num; i++){
+            for(int j=0; j <= 59; j++){
+                for(int k=0; k <= 59; k++){
+                    String hour = Integer.toString(i);
+                    String minute = Integer.toString(j);
+                    String second = Integer.toString(k);
+
+                    if(hour.contains("3") || minute.contains("3") || second.contains("3")){
+                        count++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
 }
