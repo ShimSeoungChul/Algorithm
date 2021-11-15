127c2d729a1ce9cd9574f01fd1b050e3e205d3de
Shim Seoung Chul <tlatmdcjf94@gmail.com>
2021-11-15 16:43:08 +0900

    이진탐색 - 부품찾기 문제수정

--- Books/이것이취업을위한코딩테스트다/ch07_이진탐색/부품찾기.java
+++ Books/이것이취업을위한코딩테스트다/ch07_이진탐색/부품찾기.java
@@ -1,50 +1,52 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+import java.util.Scanner;
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
+
 /*
+입력 예시
 5
 8 3 7 9 2
 3
 5 7 9
  */
 public class 부품찾기 {
-    public static void main(String [] args){
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int[] parts = new int[N];
-        for(int i=0; i<N; i++) parts[i] = scanner.nextInt();
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] parts = IntStream.range(0,N)
+            .map(num -> sc.nextInt()) // 입력
+            .sorted() // 정렬
+            .toArray();
 
-        int M = scanner.nextInt();
-        int[] selectedParts = new int[M];
-        for(int i=0; i<M; i++) selectedParts[i] = scanner.nextInt();
+        int M = sc.nextInt();
+        int[] requests =  IntStream.range(0,M)
+            .map(num -> sc.nextInt()) // 입력
+            .toArray();
 
-        부품찾기 bs = new 부품찾기();
-        bs.binarySearch(parts, selectedParts);
+        StringJoiner result = new StringJoiner(" ");
+        for(int target: requests){
+            String check = binarySearch(parts, target, 0, parts.length-1);
+            result.add(check);
+        }
+
+        System.out.println(result);
     }
 
-    public void binarySearch(int[] parts, int[] selectParts){
-        //탐색을 원하는 부품 반복
-        Arrays.stream(selectParts)
-                .forEach(x-> {
-                    int target = x;
-                    int start = 0;
-                    int end = selectParts.length;
-                    boolean isYes = false;
-
-                    //이진 탐색으로 부품 존재 확인
-                    while(start <= end){
-                        int mid = (start + end) / 2;
-                        if(target == selectParts[mid]){
-                            System.out.print("yes ");
-                            isYes = true;
-                            break;
-                        }else if(target < parts[mid]){
-                            end = mid - 1;
-                        }else{
-                            start = mid + 1;
-                        }
-                    }
-                    if(!isYes) System.out.print("no ");
-                });
+    static String binarySearch(final int[] arr, final int target, int start, int end){
+        while(start <= end){
+            final int mid = (start + end)/2;
+            if(arr[mid] == target){
+                return "yes";
+            }else if(arr[mid] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        
+        return "no";
     }
 }
