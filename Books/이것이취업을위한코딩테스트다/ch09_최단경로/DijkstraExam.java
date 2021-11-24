8777328d3156a05a4652c140fcf857cb9d2f5b96
Shim Seoung Chul <tlatmdcjf94@gmail.com>
2021-11-24 11:54:41 +0900

    refactor: 최단경로- DijkstraExam 풀이 수정

--- Books/이것이취업을위한코딩테스트다/ch09_최단경로/DijkstraExam.java
+++ Books/이것이취업을위한코딩테스트다/ch09_최단경로/DijkstraExam.java
@@ -17,102 +17,110 @@ import java.util.stream.IntStream;
 5 3 1
 5 6 2
  */
-
 class Node {
     private int index;
     private int distance;
+
     public Node(int index, int distance) {
         this.index = index;
         this.distance = distance;
     }
+
     public int getIndex() {
         return this.index;
     }
+
     public int getDistance() {
         return this.distance;
     }
 }
 
 public class DijkstraExam {
-        private static int n;
-        static int start;
-        static ArrayList<ArrayList<Node>> graph;
-        static boolean[] visited;
-        static int[] distance;
-    public static void main(String[] arsgs){
-        Scanner scanner = new Scanner(System.in);
-        //노드의 개수, 간선의 개수 입력
-        n = scanner.nextInt();
-        int m = scanner.nextInt();
-        //시작 노드 번호 입력
-        start = scanner.nextInt();
-        //각 노드에 연결되어 있는 노드에 대한 정보를 담음
-        graph = new ArrayList<>();
-        IntStream.range(0,n+1).forEach(i->graph.add(new ArrayList<Node>()));
+    static int[] distance;
+    static int start;
+    static boolean[] visited;
+    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+    static int n; // 노드의 개수
 
-        //방문 여부를 체크하는 배열
-        visited = new boolean[n+1];
-        //IntStream.range(0, visited.length).forEach(i->visited[i]=false);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 노드 n, 간선 m 개수 입력
+        n = sc.nextInt();
+        int m = sc.nextInt();
+        // 시작 노드 번호 입력
+        start = sc.nextInt();
+        // 방문 여부를 체크하는 배열
+        visited = new boolean[n + 1];
+        // 최단 거리 테이블을 모두 무한으로 초기화
+        distance = IntStream.rangeClosed(0, n)
+            .map(num -> Integer.MAX_VALUE)
+            .toArray();
 
-        //최단 거리 테이블을 모두 무한으로 초기화
-        distance = new int[n+1];
-        IntStream.range(0, distance.length).forEach(i->distance[i]=Integer.MAX_VALUE);
+        // 모든 간선 정보를 저장하는 그래프 생성
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Node>());
+        }
 
-        //모든 간선 정보를 입력
-        IntStream.range(0,m).forEach(i->{
-                    //a번 노드에서 b번 노드로 가는 비용이 c임
-                    int a = scanner.nextInt();
-                    int b = scanner.nextInt();
-                    int c = scanner.nextInt();
-                    graph.get(a).add(new Node(b,c));
-                });
+        IntStream.range(0, m)
+            .forEach(i -> {
+                // a번 노드에서 b번 노드로 가는 비용은 c
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                int c = sc.nextInt();
+                graph.get(a).add(new Node(b, c));
+            });
 
-        //다익스트라 알고리즘 수행
+        // 다익스트라 알고리즘 수행
         dijkstra();
 
-        //모든 노드로 가기 위한 최단 거리 출력
-        IntStream.rangeClosed(1, n).forEach(i->{
-            if(distance[i] == Integer.MAX_VALUE){ //도달할 수 없는 경우 무한(INFINITY) 출력
+        // 모든 노드로 가기 위한 최단 거리 출력
+        for(int i = 1; i <= n; i++){
+            // 도달할 수 없는 경우, 무한(INFINITY)라고 출력
+            if(distance[i] == Integer.MAX_VALUE){
                 System.out.println("INFINITY");
-            }else{
-                System.out.println(distance[i]); //도달할 수 있는 경우 거리를 출력
             }
-        });
+            //
+            else{
+                System.out.println(distance[i]);
+            }
+        }
     }
 
-    // 방문하지 않은 노드 중 가장 최단 거리가 짧은 노드 번호 반환
-    public static int getSmallestNode(){
+    static void dijkstra() {
+        // 시작 노드에 대해서 초기화
+        distance[start] = 0;
+        visited[start] = true;
+        ArrayList<Node> startNode = graph.get(start);
+        IntStream.range(0, startNode.size())
+            .forEach(i -> distance[startNode.get(i).getIndex()] = startNode.get(i).getDistance());
+
+        // 시작 노드를 제외한 전체 n-1 개의 노드에 대해 반복
+        for (int i = 0; i < n - 1; i++) {
+            int now = getSmallestNode();
+            visited[now] = true;
+
+            // 현재 노드와 연결된 다른 노드를 확인
+            ArrayList<Node> nowNode = graph.get(now);
+            for(int j = 0; j < nowNode.size(); j++){
+                int cost = distance[now] + nowNode.get(j).getDistance();
+                // 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+                if(cost < distance[nowNode.get(j).getIndex()]){
+                    distance[nowNode.get(j).getIndex()] = cost;
+                }
+            }
+        }
+    }
+
+    static int getSmallestNode(){
         int minValue = Integer.MAX_VALUE;
-        int index = 0; //가장 최단 거리가 짧은 노드(인덱스)
-        for(int i=0; i <= n; i++){
-            if(distance[i]<minValue && !visited[i]){
+        int index = 0; // 가장 최단 거리가 짧은 노드(인덱스)
+
+        for(int i = 1; i <= n; i++){
+            if(distance[i] < minValue && !visited[i]){
                 minValue = distance[i];
                 index = i;
             }
         }
         return index;
-    }
-
-    // 다익스트라 알고리즘
-    public static void dijkstra(){
-        //시작 노드에 대해서 초기화
-        distance[start] = 0;
-        visited[start] = true;
-        IntStream.range(0, graph.get(start).size()).forEach(i->
-                distance[graph.get(start).get(i).getIndex()] = graph.get(start).get(i).getDistance());
-
-        // 시작 노드를 제외한 전체 n-1개의 노드에 대해 반복
-        for(int i=0;i<n-1; i++){
-            // 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
-            int now = getSmallestNode();
-            visited[now] = true;
-            // 현재 노드와 연결된 다른 노드 확인
-            for(int j =0; j < graph.get(now).size(); j++){
-                int cost = distance[now] + graph.get(now).get(j).getDistance();
-                // 현재 노드를 거쳐서 다른 노드를 이동하는 거리가 더 짧은 경우
-                if(cost < distance[graph.get(now).get(j).getIndex()])
-                    distance[graph.get(now).get(j).getIndex()] = cost;
-            }
-        }
     }
 }
