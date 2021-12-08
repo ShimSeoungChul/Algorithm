c189a47d8eae2b650e995b49d77aec7205a0e8b9
Shim Seoung Chul <tlatmdcjf94@gmail.com>
2021-12-09 07:33:13 +0900

    refactor: 코드 가독성 개선

--- Books/이것이취업을위한코딩테스트다/DFSBFS/DFS_재귀.java
+++ Books/이것이취업을위한코딩테스트다/DFSBFS/DFS_재귀.java
@@ -1,40 +1,38 @@
 public class DFS_재귀 {
+    //각 노드가 방문된 정보를 1차원 배열 자료형으로 표현
+    public static boolean [] visited = {false, false, false ,false ,false ,false ,false ,false, false};
+    // 각 노드가 연결된 정보를 2차원 배열 자료형으로 표현
+    public static int[][] graph = {{},
+        {2, 3, 8},
+        {1, 7},
+        {1, 4, 5},
+        {3, 5},
+        {3, 4},
+        {7},
+        {2, 6, 8},
+        {1, 7}};
+
     public static void main(String[] args){
-
-        //각 노드가 연결된 정보를 2차원 배열 자료형으로 표현
-        int [][]graph = {{},
-                {2, 3, 8},
-                {1, 7},
-                {1, 4, 5},
-                {3, 5},
-                {3, 4},
-                {7},
-                {2, 6, 8},
-                {1, 7}};
-
-        //각 노드가 방문된 정보를 1차원 배열 자료형으로 표현
-        boolean [] visited = {false, false, false ,false ,false ,false ,false ,false, false};
-
-        //정의된 DFS 함수 호출
-        DFS_재귀 dfsExam = new DFS_재귀();
-        dfsExam.dfs(graph, 1, visited);
+        int start = 1; // 시작 노드
+        dfs(start);
     }
 
     /*
-     * dfs 메서드
-     *  @param graph 노드 연결 정보를 저장
-     *  @param v 방문을 시작하는 최상단 노드의 위치
-     *  @param visited 노드 방문 정보를 저장
+     * dfs 알고리즘을 수행하는 함수
+     * @param v 탐색할 노드
      */
-    void dfs(int [][]graph,int v, boolean [] visited){
-        //현재 노드를 방문 처리
+    public static void dfs(int v){
+        // 현재 노드 방문 처리
         visited[v] = true;
-        System.out.print(v + " ");
+        // 방문 노드 출력
+        System.out.print(v + "");
 
-        // 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-        for(int i : graph[v]){
-            if(visited[i]==false)
-                dfs(graph, i, visited);
+        // 인접 노드 탐색
+        for (int i : graph[v]){
+            // 방문하지 않은 인접 노드 중 가장 작은 노드를 스택에 넣기
+            if (visited[i]==false){
+                dfs(i);
+            }
         }
     }
 }
